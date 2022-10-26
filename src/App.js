651b6cf0d48496654cc80c1d60c4c033799754b1
arakasaka <arakasaka@gmail.com>
2022-10-26 16:04:14 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Privacy from './Privacy';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{backgroundColor: '#FF4F3F'}}>
+      <div style={{maxWidth: '800px', marginLeft: 'auto', marginRight: 'auto', color: 'white'}}>
+        <div style={{textAlign: 'center'}}>
+          <h6 style={{fontSize: 150, margin: 0}}>AR</h6>
+          <h6 style={{fontSize: 80, margin: 0}}>AKASAKA</h6>
+        </div>
+        <div style={{textAlign: 'left', marginTop: 100}}>
+          <Privacy />
+        </div>
+      </div>
     </div>
   );
 }
