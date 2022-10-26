6c4a8b9f6fd155adaa721867e38f1f06e28a921b
arakasaka <arakasaka@gmail.com>
2022-10-26 16:26:32 +0900

    update

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import Privacy from './Privacy';
 
 function App() {
   return (
-    <div style={{backgroundColor: '#FF4F3F'}}>
-      <div style={{maxWidth: '800px', marginLeft: 'auto', marginRight: 'auto', color: 'white'}}>
-        <div style={{textAlign: 'center'}}>
-          <h6 style={{fontSize: 150, margin: 0}}>AR</h6>
-          <h6 style={{fontSize: 80, margin: 0}}>AKASAKA</h6>
-        </div>
-        <div style={{textAlign: 'left', marginTop: 100}}>
+    <div style={{}}>
+      <div style={{maxWidth: '800px', marginLeft: 'auto', marginRight: 'auto', color: 'black'}}>
+        <div style={{textAlign: 'left', paddingLeft: 40, paddingRight: 40}}>
+          <h1 style={{marginBottom: 0}}>AR</h1>
+          <h1 style={{marginTop: 0}}>AKASAKA</h1>
           <Privacy />
         </div>
       </div>
