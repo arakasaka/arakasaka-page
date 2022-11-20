8576fc8d0ff55de9b123e970fe95f3be9315b1b5
arakasaka <arakasaka@gmail.com>
2022-11-20 23:14:44 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,52 @@
-import Privacy from './Privacy';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import PrivacyPolicy from './PrivacyPolicy';
+import PrivacyPolicyJp from './PrivacyPolicyJp';
 
-function App() {
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+  {
+    path: "/arakasaka",
+    element: <PageContainer><PrivacyPolicy appName="AR AKASAKA" /></PageContainer>,
+  },
+  {
+    path: "/arsocial",
+    element: <PageContainer><PrivacyPolicy appName="AR Social Media" /></PageContainer>,
+  },
+  {
+    path: "/arspot",
+    element: <PageContainer><PrivacyPolicy appName="AR Spot" /></PageContainer>,
+  },
+]);
+
+export default function App() {
   return (
-    <div style={{}}>
-      <div style={{maxWidth: '800px', marginLeft: 'auto', marginRight: 'auto', color: 'black'}}>
-        <div style={{textAlign: 'left', paddingLeft: 40, paddingRight: 40}}>
-          <h1 style={{marginBottom: 0}}>AR</h1>
-          <h1 style={{marginTop: 0}}>AKASAKA</h1>
-          <Privacy />
-        </div>
-      </div>
-    </div>
+    <RouterProvider router={router} />
+  )
+}
+
+function Root() {
+  return (
+    <PageContainer>
+      <h1 style={{marginBottom: 0}}>AR</h1>
+      <h1 style={{marginTop: 0}}>AKASAKA</h1>
+      <PrivacyPolicy appName={"AR AKASAKA, AR Social Media and AR Spot"} />
+      <PrivacyPolicyJp />
+    </PageContainer>
   );
 }
 
-export default App;
+function PageContainer(props) {
+  return (
+    <div style={{maxWidth: '800px', marginLeft: 'auto', marginRight: 'auto', color: 'black'}}>
+      <div style={{textAlign: 'left', paddingLeft: 40, paddingRight: 40}}>
+        {props.children}
+      </div>
+    </div>
+  )
+}
